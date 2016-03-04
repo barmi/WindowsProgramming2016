48d05aa5f369be64cb4feb57893eaac4b0c7adb9
skshin <skshin@gmail.com>
2016-03-04 16:29:28 +0900

    실제 내용 구현

--- week01/Account1/Account1.cpp
+++ week01/Account1/Account1.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 const int NAME_LEN = 20;
 const int MAX_ACCOUNT = 10;
+const int BASE_ACCOUNT_ID = 1000;
 
 struct Account
 {
@@ -17,15 +18,27 @@ struct Account
 	char name[NAME_LEN];    // 고객이름
 };
 
+// -------------------------------------------
+//   인터페이를 위한 함수
+// -------------------------------------------
 void PrintMenu(); // 메뉴출력 
 void MakeAccount(); // 계좌 개설을 위한 함수 
 void Deposit(); // 입 금 
 void Withdraw(); // 출 금 
 void Remainder(); // 잔액 조회 
 
+
+// -------------------------------------------
+//   전역 변수
+// -------------------------------------------
 int g_menu;
-int g_account[MAX_ACCOUNT];
+Account g_account[MAX_ACCOUNT];
 int g_cur_account = 0;
+
+// -------------------------------------------
+//   로컬 함수
+// -------------------------------------------
+void PrintAccount(int id);
 
 int main()
 {
@@ -49,6 +62,8 @@ int main()
 		}
 	} while (g_menu != 9);
 
+	system("pause");
+
 	return 0;
 }
 
@@ -70,20 +85,76 @@ void PrintMenu()
 // 계좌 개설을 위한 함수
 void MakeAccount()
 {
+	if (g_cur_account == MAX_ACCOUNT)
+	{
+		cout << "[더이상 계좌를 개설할 수 없습니다.]";
+		return;
+	}
+	
+	cout << "* 계좌개설을 위해서 다음 내용을 입력해 주세요." << endl;
+	cout << " - 이름 : ";
+	cin >> g_account[g_cur_account].name;
+	cout << " - 초기 입금액 : ";
+	cin >> g_account[g_cur_account].balance;
 
+	g_account[g_cur_account].id = BASE_ACCOUNT_ID + g_cur_account;
+	++g_cur_account;
+	cout << "[계좌를 개설했습니다.]" << endl;
 }
 
 // 입 금 
 void Deposit()
 {
+	int user_id, user_money;
+	cout << "* 입금을 위해서 다음 내용을 입력해 주세요." << endl;
+	cout << " - 계좌번호 : ";
+	cin >> user_id;
+	cout << " - 입금액 : ";
+	cin >> user_money;
+	
+	if ((user_id < BASE_ACCOUNT_ID) || (user_id > BASE_ACCOUNT_ID + g_cur_account))
+	{
+		cout << "[계좌번호를 잘 못 입력했습니다. 확인 후 다시 시도해 주시기 바랍니다.]" << endl;
+		return;
+	}
+	g_account[user_id - BASE_ACCOUNT_ID].balance += user_money;
+
+	PrintAccount(user_id);
 }
 
 // 출 금 
 void Withdraw()
 {
+	int user_id, user_money;
+	cout << "* 출금을 위해서 다음 내용을 입력해 주세요." << endl;
+	cout << " - 계좌번호 : ";
+	cin >> user_id;
+	cout << " - 출금액 : ";
+	cin >> user_money;
+
+	if ((user_id < BASE_ACCOUNT_ID) || (user_id > BASE_ACCOUNT_ID + g_cur_account))
+	{
+		cout << "[계좌번호를 잘 못 입력했습니다. 확인 후 다시 시도해 주시기 바랍니다.]" << endl;
+		return;
+	}
+	g_account[user_id - BASE_ACCOUNT_ID].balance -= user_money;
+
+	PrintAccount(user_id);
 }
 
 // 잔액 조회 
 void Remainder()
 {
+	for (int i = 0; i < g_cur_account; ++i)
+	{
+		PrintAccount(BASE_ACCOUNT_ID + i);
+	}
+}
+
+void PrintAccount(int id)
+{
+	cout << " - 계좌번호:" << g_account[id - BASE_ACCOUNT_ID].id
+		<< ", 이름:" << g_account[id - BASE_ACCOUNT_ID].name
+		<< ", 잔고:" << g_account[id - BASE_ACCOUNT_ID].balance
+		<< endl;
 }
